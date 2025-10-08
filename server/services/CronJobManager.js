a506579b689fc6bfa1e63286a809b54c8f11c8bb
Bohdan Motrych <bohdanmotrych8@gmail.com>
2025-10-08 06:57:41 +0000

    commit

--- server/services/CronJobManager.js
+++ server/services/CronJobManager.js
@@ -154,7 +154,7 @@ class CronJobManager {
         try {
             // Get the last ads sync date to use as start date
             let { adsStartDate, endDate } = await this.getLastSyncDates(storeId);
-            endDate = new Date(endDate.getTime() + 1000 * 60 * 60 * 48)
+            endDate = new Date(endDate.getTime() + 1000 * 60 * 60 * 24 * 2)
             console.log("syncingAds", adsStartDate, endDate)
             if (!adsStartDate) {
                 adsStartDate = new Date(2023, 1, 1);
